54eec30caa2ef655f6e3f9d2309b26a83534fd2c
seleneneen-debug <seleneneen@gmail.com>
2025-11-25 02:14:38 +0500

    Fix join button - don't block on Firebase

--- client/src/components/JoinScreen.jsx
+++ client/src/components/JoinScreen.jsx
@@ -1,32 +1,5 @@
 import React, { useState } from 'react';
 import { db } from '../firebase-config';
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-
-function JoinScreen({ joinChat }) {
-    const [username, setUsername] = useState("");
-
-    const handleJoin = async () => {
-        if (username !== "") {
-            // Send "User Joined" system message
-            try {
-                await addDoc(collection(db, "messages"), {
-                    message: `${username} joined the chat`,
-                    author: "System",
-                    createdAt: serverTimestamp(),
-                    time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
-                });
-            } catch (error) {
-                console.error("Error sending join message:", error);
-            }
-
-            joinChat(username);
-        }
-    };
-
-    return (
-        <div className="join-container">
-            <div className="join-card">
-                <h1>Global Chat</h1>
                 <p>Enter your name to join everyone</p>
                 <div className="input-group">
                     <input
@@ -39,8 +12,8 @@ function JoinScreen({ joinChat }) {
                     />
                 </div>
                 <button onClick={handleJoin}>Join Chat</button>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
